2bd4914133caaed452376ee2b1844e01a9c51494
Musharraf <musharraf.personal@gmail.com>
2023-02-06 15:34:34 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var MainImg = document.getElementById("Main-Img");
+var smallimg = document.getElementsByClassName("small-img");
+    smallimg[0].onclick = function(){
+        MainImg.src = smallimg[0].src;
+    }
+    smallimg[1].onclick = function(){
+        MainImg.src = smallimg[1].src;
+    }
+    smallimg[2].onclick = function(){
+        MainImg.src = smallimg[2].src;
+    }
+    smallimg[3].onclick = function(){
+        MainImg.src = smallimg[3].src;
+    }
+    smallimg[4].onclick = function(){
+        MainImg.src = smallimg[4].src;
+    }
+    smallimg[5].onclick = function(){
+        MainImg.src = smallimg[5].src;
+    }
